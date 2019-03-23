3b4e9591eb14d32baf26d12b3dc5811d8cebcf73
Sabrina Wallace <wallas4@unlv.nevada.edu>
2019-03-22 19:57:11 -0700

    Added ResultsPage to main game project

---
+++ MathWithFriends/app/src/main/java/com/example/mathwithfriends/ResultsActivity.java
@@ -0,0 +1,50 @@
+package com.example.mathwithfriends;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+import android.widget.Toast;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class ResultsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton() {
+
+        ImageButton playAgain = (ImageButton) findViewById(R.id.playAgainButton);
+        ImageButton homeButton = (ImageButton) findViewById(R.id.homeButton);
+
+        playAgain.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Toast.makeText(ResultsActivity.this,
+                        "Let's Play Again!", Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
+
+        homeButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Toast.makeText(ResultsActivity.this,
+                        "Let's Go Home Then...", Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
+
+    }
+}
+
