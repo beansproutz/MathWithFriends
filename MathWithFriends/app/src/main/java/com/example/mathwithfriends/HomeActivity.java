2e8ba8b390e06dd17399deccab0ed735d6776faa
Zachary Waddington <waddiz1@unlv.nevada.edu>
2019-03-24 15:11:26 -0700

    added 'HomeActivity' java class

---
+++ MathWithFriends/app/src/main/java/com/example/mathwithfriends/HomeActivity.java
@@ -0,0 +1,27 @@
+package com.example.mathwithfriends;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomeActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+    
+}
