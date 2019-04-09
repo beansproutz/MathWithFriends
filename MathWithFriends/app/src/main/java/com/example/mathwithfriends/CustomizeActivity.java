b2bcddd6d5dce5a52f61d936b1ff3ac6686f986c
Leslie Tran <tranl2@unlv.nevada.edu>
2019-04-08 17:16:29 -0700

    git rm misc.xml and vcs.xml because they're giving me errors when I want to git pull

--- MathWithFriends/app/src/main/java/com/example/mathwithfriends/CustomizeActivity.java
+++ MathWithFriends/app/src/main/java/com/example/mathwithfriends/CustomizeActivity.java
@@ -9,8 +9,11 @@ import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class CustomizeActivity extends AppCompatActivity
         implements View.OnClickListener {
@@ -25,8 +28,10 @@ public class CustomizeActivity extends AppCompatActivity
     private ImageButton iconButton8;
     private Button customToHomeButton;  // Return to home page button
 
-    FirebaseAuth mAuth;
-    DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;         // To get user id
+    private DatabaseReference mDatabase;// To write to avatarID field
+
+    private Integer currAvatar;         // Currently chosen avatar, updated as user presses buttons
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +43,21 @@ public class CustomizeActivity extends AppCompatActivity
         // Initialize Firebase stuffs.
         mAuth = FirebaseAuth.getInstance();
         mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Users").child(mAuth.getUid());
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get user's current avatar from Firebase.
+                User user = dataSnapshot.getValue(User.class);
+                currAvatar = user.getAvatarID();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("Error: Can't retrieve avatar data");
+            }
+        });
 
         iconButton1 = findViewById(R.id.userIcon1);
         iconButton1.setOnClickListener(this);
@@ -62,50 +82,52 @@ public class CustomizeActivity extends AppCompatActivity
     @Override
     public void onClick(View view) {
         // Perform tasks based on which button is pressed. If an icon
-        // button is pressed, inform user and set icon. If home button
-        // is pressed, return to home page.
+        // button is pressed, inform user and store the corresponding
+        // icon number temporarily. If home button is pressed, update the
+        // avatarID field in Firebase return to home page.
         switch(view.getId()) {
             case R.id.userIcon1:
-                mDatabase.child("Users").child(mAuth.getUid()).child("avatarID").setValue(1);
+                currAvatar = 1;
                 Toast.makeText(CustomizeActivity.this,
                         "Icon 1 chosen!", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.userIcon2:
-                mDatabase.child("Users").child(mAuth.getUid()).child("avatarID").setValue(2);
+                currAvatar = 2;
                 Toast.makeText(CustomizeActivity.this,
                         "Icon 2 chosen!", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.userIcon3:
-                mDatabase.child("Users").child(mAuth.getUid()).child("avatarID").setValue(3);
+                currAvatar = 3;
                 Toast.makeText(CustomizeActivity.this,
                         "Icon 3 chosen!", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.userIcon4:
-                mDatabase.child("Users").child(mAuth.getUid()).child("avatarID").setValue(4);
+                currAvatar = 4;
                 Toast.makeText(CustomizeActivity.this,
                         "Icon 4 chosen!", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.userIcon5:
-                mDatabase.child("Users").child(mAuth.getUid()).child("avatarID").setValue(5);
+                currAvatar = 5;
                 Toast.makeText(CustomizeActivity.this,
                         "Icon 5 chosen!", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.userIcon6:
-                mDatabase.child("Users").child(mAuth.getUid()).child("avatarID").setValue(6);
+                currAvatar = 6;
                 Toast.makeText(CustomizeActivity.this,
                         "Icon 6 chosen!", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.userIcon7:
-                mDatabase.child("Users").child(mAuth.getUid()).child("avatarID").setValue(7);
+                currAvatar = 7;
                 Toast.makeText(CustomizeActivity.this,
                         "Icon 7 chosen!", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.userIcon8:
-                mDatabase.child("Users").child(mAuth.getUid()).child("avatarID").setValue(8);
+                currAvatar = 8;
                 Toast.makeText(CustomizeActivity.this,
                         "Icon 8 chosen!", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.custom2home:
+                mDatabase.child("Users").child(mAuth.getUid()).child("avatarID").setValue(currAvatar);
                 Intent intent = new Intent(CustomizeActivity.this, HomeActivity.class);
                 startActivity(intent);
                 break;
