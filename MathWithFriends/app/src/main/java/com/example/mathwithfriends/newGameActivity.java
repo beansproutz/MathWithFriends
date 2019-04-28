e361f773cbc2d591f0105c203c61734d268a9a3e
tommy567295 <truont1@unlv.nevada.edu>
2019-04-27 22:29:56 -0700

    Started the design .xml for the newGame design.

---
+++ MathWithFriends/app/src/main/java/com/example/mathwithfriends/newGameActivity.java
@@ -0,0 +1,14 @@
+package com.example.mathwithfriends;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class newGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_game);
+    }
+
+}
